c88e5959613e61e1e71110837937ab5d69062898
Prabir Kundu <prabirkundu222@gmail.com>
2024-10-10 22:20:28 +0530

    commit by Prabir 10-10-2024 1) FibonacciSeriesWithRecursion.java

--- src/FibonacciSeriesWithRecursion.java
+++ src/FibonacciSeriesWithRecursion.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class FibonacciSeriesWithRecursion {
     static int n1 = 0, n2 = 1, n3, i=0, count=0;
     static void printFibonacci(int count){
-        if (i < count){
+        if (i < count) {
             n3 = n1 + n2;
             n1 = n2;
             n2 = n3;
