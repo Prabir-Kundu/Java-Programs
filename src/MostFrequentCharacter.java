ffb02b4999ca35429475d3c3f8a9d9af1068ba81
Prabir Kundu <prabirkundu222@gmail.com>
2025-02-23 20:13:45 +0530

    commit by Prabir 23-02-2025

--- src/MostFrequentCharacter.java
+++ src/MostFrequentCharacter.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class MostFrequentCharacter {
     public static void main(String[] args) {
-        String str = "abbcab";
+        String str = "aabbcab";
         findMostFrequentChar(str);
     }
 
@@ -18,15 +18,35 @@ public class MostFrequentCharacter {
         // Find the most frequent character
         char mostFrequentChar = '\0';
         int maxCount = 0;
-
-        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+        String str1="";
+        /*for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
             if (entry.getValue() > maxCount) {
                 mostFrequentChar = entry.getKey();
                 maxCount = entry.getValue();
             }
+        }*/
+
+
+        for(Map.Entry<Character, Integer> entry: charCountMap.entrySet()){
+            if(entry.getValue() == maxCount){
+                mostFrequentChar = entry.getKey();
+                maxCount = entry.getValue();
+
+                if(str1.isEmpty()){
+                    str1 = ""+mostFrequentChar;
+                } else {
+                    str1 += ","+mostFrequentChar;
+                }
+
+            } else if(entry.getValue() > maxCount) {
+                mostFrequentChar = entry.getKey();
+                maxCount = entry.getValue();
+                str1 = ""+mostFrequentChar;
+            }
         }
 
-        System.out.println("Most Repetitive Character: " + mostFrequentChar);
+        //System.out.println("Most Repetitive Character: " + mostFrequentChar);
+        System.out.println("Most Repetitive Character: " + str1);
         System.out.println("Count: " + maxCount);
     }
 }
